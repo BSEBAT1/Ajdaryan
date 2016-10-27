2c63cb6fa5369dc4976778edf5c6031de2c7cb63
Berkay <Berkay_Sebat@yahoo.com>
2016-10-27 10:57:52 -0400

    initial commit

---
+++ helloworld.h
@@ -0,0 +1,13 @@
+//
+//  helloworld.h
+//  Sourecontrol
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.source. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface helloworld : UIView
+
+@end
---
+++ helloworld.m
@@ -0,0 +1,21 @@
+//
+//  helloworld.m
+//  Sourecontrol
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.source. All rights reserved.
+//
+
+#import "helloworld.h"
+
+@implementation helloworld
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

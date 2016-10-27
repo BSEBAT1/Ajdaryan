0332fd6ca08b0ea74e61ccfd835dee491d5e1d28
Berkay <Berkay_Sebat@yahoo.com>
2016-10-27 11:28:31 -0400

    created 4 new files

---
+++ Sourecontrol/newnewclass.m
@@ -0,0 +1,21 @@
+//
+//  newnewclass.m
+//  Sourecontrol
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.source. All rights reserved.
+//
+
+#import "newnewclass.h"
+
+@implementation newnewclass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
